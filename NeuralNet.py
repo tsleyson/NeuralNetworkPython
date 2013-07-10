fd48b51f7b893d1e924a06489dbc49fb97897f63
Trisdan Leyson <tsleyson@ucdavis.edu>
2013-07-09 20:15:04 -0700

    First commit of initial NeuralNet file.

---
+++ NeuralNet.py
@@ -0,0 +1,5 @@
+# NeuralNet.py
+# A Python implementation of a neural network to distinguish males
+# from females. Hoping to get better than that lousy 50% accuracy
+# that the Java version had. Using Python cause it's easy and nice.
+import numpy as np
