94c416efef4a9e7b73c18d4396ecd801a170c1c5
Trisdan Leyson <tsleyson@ucdavis.edu>
2013-07-11 21:51:46 -0700

    Added argument parsing.

--- NeuralNet.py
+++ NeuralNet.py
@@ -2,4 +2,12 @@
 # A Python implementation of a neural network to distinguish males
 # from females. Hoping to get better than that lousy 50% accuracy
 # that the Java version had. Using Python cause it's easy and nice.
-import numpy as np
+import sys, os, argparse, numpy as np
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="Takes train, test, and verify options.")
+    args.add_argument("-train", nargs=2)
+    args.add_argument("-test", action=store)
+    args.add_argument("-verify", action=store_true)
+    argnames = args.parse_args()
+    print(argnames)
