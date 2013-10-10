b5130ac9bc1f0d3d911516094e9ebbd0ec204a7a
Trisdan Leyson <tsleyson@ucdavis.edu>
2013-10-10 14:05:53 -0700

    Finally got rid of redundant files

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,4 +1,4 @@
-# NeuralNet.py
+# NeuralNet2.py
 # A Python implementation of a neural network to distinguish males
 # from females. Hoping to get better than that lousy 50% accuracy
 # that the Java version had. Using Python cause it's easy and nice.
@@ -12,23 +12,31 @@ class Network:
     	numnodes is a list of the number of nodes in each layer, including the input layer 
     	and the output layer. numlayers excludes the input layer.
     	"""
-    	assert len(numnodes) ==  numlayers - 1
+    	assert len(numnodes) - 1  ==  numlayers
     	self.layers = [Layer(numnodes[i-1], numnodes[i]) for i in range(1, len(numnodes))]
     
     def feed_network(self, inputs):
-		"""
-		inputs is a numpy array of input values.
-		"""
-		currentin = inputs
-		for layer in self.layers:
-			print(layer.__repr__())
-			currentin = layer.calc_outputs(currentin)
-		self.output = currentin
-		return self.output
+        """
+	inputs is a numpy array of input values. Each index of
+        the input value must correspond to one input neuron.
+	"""
+        currentin = inputs
+        for layer in self.layers:
+            if debug: print(layer.__repr__())
+            currentin = layer.calc_outputs(currentin)
+        self.output = currentin
+        return self.output
 
     def propagate_back(example, answer):
-        out = self.feed_network(example)[0]	# Assumes one output
-        error = out - answer
+        """
+        example is the data to give to feed_network
+        answer is a numpy array of the correct outputs for this example.
+        """
+        self.feed_network(example)
+        outvals = self.layers[-1].output
+        delta_h = outvals * (np.ones(len(outvals)) - outvals) * (answer - outvals)
+        
+        
 #end Network
 
 class Layer:
@@ -53,15 +61,18 @@ class Layer:
     	assert len(self.weights) == len(inputs), "Layer.calc_outputs: mismatched inputs."
     	self.weightedsums = np.dot(inputs, self.weights)
         self.output = np.array(map(lambda x : 1/(1 + math.exp(-x)), self.weightedsums))
-        return self.output  # Badly thought out, having it both ways design.
+        # Note: might need to add a w_0 weight.
+        return self.output  
+        # Both save and return for debugging purposes (to see the dimensionality)
 
-	def calc_delta(isOutput=False):
-		"""
-		Call as calc_delta(isOutput=True) if the layer it's being called on is an output layer;
-		otherwise call as calc_delta().
-		"""
-		assert self.output != None
-		
+    def calc_delta(isOutput=False):
+        """
+	Call as calc_delta(isOutput=True) if the layer it's being called on is an output layer;
+	otherwise call as calc_delta().
+	"""
+        assert self.output != None
+        if isOutput:
+            return 
 
     def __repr__(self):
     	return "Layer(inputs={0}, nodes={1})".format(self.numInputs, self.numNodes)
@@ -87,8 +98,9 @@ if __name__ == "__main__":
     	n = Network(4, [3, 2, 1])
     	print(n.layers)
     	print(n.feed_network(np.array([0, 1, 1])))
+
  
- 	# When it's time to read in data, do it like this. But with functions.
+    # When it's time to read in data, do it like this. But with functions.
     #fname = os.path.join(argvals.train[0], os.listdir(argvals.train[0])[0])
     #indata = np.fromfile(fname, dtype=int, count=-1, sep=' ')
     #print(indata, " ", indata.shape)
@@ -104,3 +116,9 @@ if __name__ == "__main__":
 # function of n inputs. Before we had like 30. We have 1024 inputs. No wonder it couldn't get
 # above 50% accuracy. If an input can have 128 values (seems like the pixels use 8-bit greyscale)
 # and there are 1024 inputs, we'd need about 128^1024 / 1024 =~ 5.93e2154 hidden units.
+
+#Note: this works:
+# a = [1, 2, 3]
+# b = [5, 6, 7]
+# c = [9, 10, 11]
+# map(lambda x, y, z: x+y+z, a, b, c)
--- NeuralNetTest.py
+++ NeuralNetTest.py
@@ -1,16 +1,12 @@
 # NeuralNetTests.py
 # Unit tests for the neural network class. Mostly I wanted to play with
 # the unittest module, but if we don't need this, we don't need it.
-import unittest, NeuralNet
+import unittest, NeuralNet, numpy
 
-class NetTestBasic(unittest.TestCase):
+class TestNetwork(unittest.TestCase):
 	def setUp(self):
-		self.network = NeuralNet.Network(4, [2, 3, 1])
-
-class TestFeedByPrinting(NetTestBasic):
-	def runTest(self):
-		print(self.network.feed_network(np.array[0,1]))
-
+                self.network = NeuralNet.Network(3, [2, 2, 2, 2])
+        def test_feed(self):
+                print(self.network.feed_network(numpy.array([0, 0])))
 if __name__ == "__main__":
-	n = TestFeedByPrinting()
-	n.runTest()
+	unittest.main()
